9f10f8744d12c805e644e9893dc799d4e9da75d5
Youli Tuo <crazuni1993@gmail.com>
2020-05-16 13:39:12 +0800

    bug fixed

--- LE_saturation_cor.py
+++ LE_saturation_cor.py
@@ -89,25 +89,25 @@ def select_oneboxtime(time, boxnum=0, **kwargs):
     channelmin = kwargs['minPI']
     channelmax = kwargs['maxPI']
 
-#    new_time = time[(np.isin(detid,le_small_detid))&
-#            (channel>=channelmin)&
-#            (channel<=channelmax)&
-#            (detbox==boxnum)]
-    if boxnum == 0:
-        new_time = time[(np.isin(detid,le_small_detid))&
-                (channel>=channelmin)&
-                (channel<=channelmax)&
-                (detid<=31)]
-    elif boxnum == 1:
-        new_time = time[(np.isin(detid,le_small_detid))&
-                (channel>=channelmin)&
-                (channel<=channelmax)&
-                (detid>=32)&(detid<=63)]
-    elif boxnum == 2:
-        new_time = time[(np.isin(detid,le_small_detid))&
-                (channel>=channelmin)&
-                (channel<=channelmax)&
-                (detid>=64)]
+    new_time = time[(np.isin(detid,le_small_detid))&
+            (channel>=channelmin)&
+            (channel<=channelmax)&
+            (detbox==boxnum)]
+#    if boxnum == 0:
+#        new_time = time[(np.isin(detid,le_small_detid))&
+#                (channel>=channelmin)&
+#                (channel<=channelmax)&
+#                (detid<=31)]
+#    elif boxnum == 1:
+#        new_time = time[(np.isin(detid,le_small_detid))&
+#                (channel>=channelmin)&
+#                (channel<=channelmax)&
+#                (detid>=32)&(detid<=63)]
+#    elif boxnum == 2:
+#        new_time = time[(np.isin(detid,le_small_detid))&
+#                (channel>=channelmin)&
+#                (channel<=channelmax)&
+#                (detid>=64)]
     return new_time
 
 def genlc_forcedtrigger(time_evt, evttype, detbox, **kwargs):
@@ -126,24 +126,28 @@ def genlc_forcedtrigger(time_evt, evttype, detbox, **kwargs):
     return forcedtrigger_lc_x, forcedtrigger_lc_y
 
 def coeff_forcedtrigger_rate(time, rate, detboxid=0):
+    coeff = np.array([1000./x if x>0 else 0 for x in rate])
+    return coeff
+
+def coeff_detnumnorm(time, fte_rate, detboxid=0):
     if detboxid == 0:
         detnum = 19
     if detboxid == 1:
         detnum = 20
     if detboxid == 2:
         detnum = 19
-    coeff = np.array([1000./x if x>0 else 0 for x in rate])
+    coeff = np.array([1/detnum if x>0 else 0 for x in fte_rate])
     return coeff
 
 def detnum_cor(time, *args):
 
     for i,rate in enumerate(args):
         if i == 0:
-            coeff0 = np.array([19 if x>0 else 0 for x in rate])
+            coeff0 = np.array([1 if x>0 else 0 for x in rate])
         elif i == 1:
-            coeff1 = np.array([20 if x>0 else 0 for x in rate])
+            coeff1 = np.array([1 if x>0 else 0 for x in rate])
         elif i == 2:
-            coeff2 = np.array([19 if x>0 else 0 for x in rate])
+            coeff2 = np.array([1 if x>0 else 0 for x in rate])
     detnum_eff = coeff0 + coeff1 + coeff2
     return detnum_eff
 
@@ -179,6 +183,10 @@ def saturation_cor(screenfile, evtfile, **kwargs):
     lc_x_detbox1, lc_y_detbox1 = genlc_bin(time_detbox1, bins=np.arange(tstart, tstop, time_resolution))
     lc_x_detbox2, lc_y_detbox2 = genlc_bin(time_detbox2, bins=np.arange(tstart, tstop, time_resolution))
 
+#    fig, (ax1,ax2) = plt.subplots(2,1, sharex=True)
+#    ax1.errorbar(lc_x_detbox0, lc_y_detbox0, drawstyle="steps-mid")
+#    ax1.errorbar(lc_x_detbox1, lc_y_detbox1, drawstyle="steps-mid")
+#    ax1.errorbar(lc_x_detbox2, lc_y_detbox2, drawstyle="steps-mid")
 
     #generate LC for forced trigger event based on bins of each detbox LC
     fte_lc_x_detbox0, fte_lc_y_detbox0 = genlc_forcedtrigger(time_evt, evttype, detbox=detbox_evt, starttime=tstart, stoptime=tstop,
@@ -192,21 +200,24 @@ def saturation_cor(screenfile, evtfile, **kwargs):
     coeff0 = coeff_forcedtrigger_rate(fte_lc_x_detbox0, fte_lc_y_detbox0, detboxid=0)
     coeff1 = coeff_forcedtrigger_rate(fte_lc_x_detbox1, fte_lc_y_detbox1, detboxid=1)
     coeff2 = coeff_forcedtrigger_rate(fte_lc_x_detbox2, fte_lc_y_detbox2, detboxid=2)
+    coeff_detnorm0 = coeff_detnumnorm(fte_lc_x_detbox0, fte_lc_y_detbox0, detboxid=0)
+    coeff_detnorm1 = coeff_detnumnorm(fte_lc_x_detbox1, fte_lc_y_detbox1, detboxid=1)
+    coeff_detnorm2 = coeff_detnumnorm(fte_lc_x_detbox2, fte_lc_y_detbox2, detboxid=2)
 
     detnum_cor_coeff = detnum_cor(fte_lc_x_detbox0, fte_lc_y_detbox0, fte_lc_y_detbox1, fte_lc_y_detbox2)
 
     time_corrected = lc_x_detbox0
-    rate_corrected = (lc_y_detbox0 * coeff0/19 + 
-        lc_y_detbox1 * coeff1/20 + 
-        lc_y_detbox2 * coeff2/19)*58/(detnum_cor_coeff)  #NORMALIZED for detector number
+    rate_corrected = (lc_y_detbox0 * coeff0 * coeff_detnorm0 + 
+        lc_y_detbox1 * coeff1 * coeff_detnorm1+ 
+        lc_y_detbox2 * coeff2 * coeff_detnorm2)*58/detnum_cor_coeff
 
     #ERROR
     error_detbox0 = np.sqrt(lc_y_detbox0/time_resolution)
     error_detbox1 = np.sqrt(lc_y_detbox1/time_resolution)
     error_detbox2 = np.sqrt(lc_y_detbox2/time_resolution)
-    error_corrected = 58*np.sqrt((error_detbox0*coeff0/detnum_cor_coeff/19)**2 + 
-            (error_detbox1*coeff1/detnum_cor_coeff/20)**2 + 
-            (error_detbox2*coeff2/detnum_cor_coeff/19)**2 )
+    error_corrected = 58*np.sqrt((error_detbox0*coeff0*coeff_detnorm0/detnum_cor_coeff)**2 + 
+            (error_detbox1*coeff1*coeff_detnorm1/detnum_cor_coeff)**2 + 
+            (error_detbox2*coeff2*coeff_detnorm2/detnum_cor_coeff)**2 )
     #NOTE:use propogating error instead of counts error.
     #error_corrected = np.sqrt(rate_corrected/time_resolution)
 
